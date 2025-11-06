2bcef6be0774889c40ab449821becbd2f24e4a89
Piyush Sindhu <piyush124790@gmail.com>
2025-11-06 22:42:35 +0530

    Question 8 Of C Programming
    Write a Program To Use While Loop To Print Fibonacci series Up To N Terms.

---
+++ C Programming (All Files)/Building Blocks Of Programming/Question_8.c
@@ -0,0 +1,28 @@
+// Write a Program To Use While Loop To Print Fibonacci series Up To N Terms.
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int i = 0;
+    int value1 = 0;
+    int value2 = 1;
+    int sum;
+
+    printf("Please Enter The Number Of Terms Of The Fibonacci Series : ");
+    scanf("%d", &num);
+
+    printf("Fibonacci Series Up To %d Terms : ", num);
+
+    while (i < num)
+    {
+        printf("%d ", value1);
+        sum = value1 + value2;
+        value1 = value2;
+        value2 = sum;
+        i++;
+    }
+
+    return 0;
+}
