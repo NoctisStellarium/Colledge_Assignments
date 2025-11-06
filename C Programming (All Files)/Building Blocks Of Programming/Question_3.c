9143bbf4da4465f250728cebd5d5f12bc334bd65
Piyush Sindhu <piyush124790@gmail.com>
2025-11-06 22:41:09 +0530

    Question 3 Of C Programming
    Write a Program To Demonstrate Shadowing of a Global Variable by a Local Variable.

---
+++ C Programming (All Files)/Building Blocks Of Programming/Question_3.c
@@ -0,0 +1,24 @@
+// Write a Program To Demonstrate Shadowing of a Global Variable by a Local Variable.
+
+#include <stdio.h>
+
+void function()
+{
+    int global = 10;
+
+    printf("The Original Global Variable Is : %d", global);
+}
+
+int main()
+{
+    int global;
+
+    printf("Please Enter a Number That Will Shadow The Global Variable : ");
+    scanf("%d", &global);
+
+    printf("The Local Variable You Entered Is : %d\n", global);
+
+    function();
+
+    return 0;
+}
