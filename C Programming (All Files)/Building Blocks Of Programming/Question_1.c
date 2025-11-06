9a16cdb3d41a87457a91a8dd4a8c6887c968671e
Piyush Sindhu <piyush124790@gmail.com>
2025-11-06 22:40:37 +0530

    Question 1 Of C Programming
    Write a Program To Declare and Define an Integer Variable and Print it.

---
+++ C Programming (All Files)/Building Blocks Of Programming/Question_1.c
@@ -0,0 +1,15 @@
+// Write a Program To Declare and Define an Integer Variable and Print it.
+
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    printf("Please Enter a Number You Want To Print : ");
+    scanf("%d", &number);
+
+    printf("The Integral Number You Entered Is : %d", number);
+
+    return 0;
+}
