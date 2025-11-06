99b22efc1e6a1c69cf7e7c8ce704bd0e276955ee
Piyush Sindhu <piyush124790@gmail.com>
2025-11-06 22:43:50 +0530

    Question 12 Of C Programming
    Write a Program To Use a Function that Returns the Larger of Two Numbers (Arguments, Return Value).

---
+++ C Programming (All Files)/Building Blocks Of Programming/Question_12.c
@@ -0,0 +1,38 @@
+// Write a Program To Use a Function that Returns the Larger of Two Numbers (Arguments, Return Value).
+
+#include <stdio.h>
+
+int LargerNumber(int a, int b)
+{
+    if (a > b)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    int num1;
+    int num2;
+
+    printf("Please Enter The First Number : ");
+    scanf("%d", &num1);
+
+    printf("Please Enter The Second Number : ");
+    scanf("%d", &num2);
+
+    if (LargerNumber(num1, num2))
+    {
+        printf("The Larger Number Is : %d", num1);
+    }
+    else
+    {
+        printf("The Larger Number Is : %d", num2);
+    }
+
+    return 0;
+}
