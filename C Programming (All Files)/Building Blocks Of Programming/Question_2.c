e54392e916a39453769d1e96d67b6f260bae0a71
Piyush Sindhu <piyush124790@gmail.com>
2025-11-06 22:40:55 +0530

    Question 2 Of C Programming
    Write a Program To Declare a Float Without Initializing and Later Assign a Value.

---
+++ C Programming (All Files)/Building Blocks Of Programming/Question_2.c
@@ -0,0 +1,15 @@
+// Write a Program To Declare a Float Without Initializing and Later Assign a Value.
+
+#include <stdio.h>
+
+int main()
+{
+    float number;
+
+    printf("Please Enter a Number You Want To Print : ");
+    scanf("%f", &number);
+
+    printf("The Floating Number You Entered Is : %.2f", number);
+
+    return 0;
+}
