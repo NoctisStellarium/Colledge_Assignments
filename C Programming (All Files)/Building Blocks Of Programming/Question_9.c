f2085ec49392f9203d88ff93cf44ac12e46cb68d
Piyush Sindhu <piyush124790@gmail.com>
2025-11-06 22:42:50 +0530

    Question 9 Of C Programming
    Write a Program To Use Do While Loop To Read Numbers Until User Enters 0.

---
+++ C Programming (All Files)/Building Blocks Of Programming/Question_9.c
@@ -0,0 +1,24 @@
+// Write a Program To Use Do While Loop To Read Numbers Until User Enters 0.
+
+#include <stdio.h>
+
+int main()
+{
+    int number;
+
+    do
+    {
+        printf("Please Enter Zero To Exit The Loop : ");
+        scanf("%d", &number);
+
+        if (number != 0)
+        {
+            printf("The Number You Entered Is : %d\n", number);
+        }
+
+    } while(number != 0);
+
+    printf("Program Terminated");
+
+    return 0;
+}
