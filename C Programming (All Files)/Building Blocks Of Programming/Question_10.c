b20c2fa5522d8619102817f7ef3c100a823e13fd
Piyush Sindhu <piyush124790@gmail.com>
2025-11-06 22:43:17 +0530

    Question 10 Of C Programming
    Write a Program To Use a Function to Check If a Number is Prime (Arguments, Return Value).

---
+++ C Programming (All Files)/Building Blocks Of Programming/Question_10.c
@@ -0,0 +1,46 @@
+// Write a Program To Use a Function to Check If a Number is Prime (Arguments, Return Value).
+
+#include <stdio.h>
+#include <math.h>
+
+int CheckPrime(int x)
+{
+    if (x <= 1)
+    {
+        return 0;
+    }
+    else
+    {
+        for (int i = 2; i <= sqrt(x); i++)
+        {
+            if (x % i == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    int number;
+
+    printf("Please Enter a Number To Check Whether It Is Prime Or Not : ");
+    scanf("%d", &number);
+
+    if (CheckPrime(number))
+    {
+        printf("%d Is a Prime Number", number);
+    }
+    else
+    {
+        printf("%d Is Not a Prime Number", number);
+    };
+
+    return 0;
+}
