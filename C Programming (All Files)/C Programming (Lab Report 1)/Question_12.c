7f4fa7a36081b0d2832247f778cc24be0b9cc8bc
Piyush Sindhu <piyush124790@gmail.com>
2025-11-09 22:51:50 +0530

    Question 12 Of C Programming
    Write a C Program To Enter Numbers Till The User Wants. At the End, It Should Display The Count Of Positive, Negative, and Zeroes Entered.

---
+++ C Programming (All Files)/C Programming (Lab Report 1)/Question_12.c
@@ -0,0 +1,40 @@
+// Write a C Program To Enter Numbers Till The User Wants. At the End, It Should Display The Count Of Positive, Negative, and Zeroes Entered.
+
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    int value;
+    int positive = 0;
+    int negative = 0;
+    int zero = 0;
+
+    printf("Please Enter How Many Numbers You Want To Input : ");
+    scanf("%d", &number);
+
+    for (int i = 1; i <= number; i++)
+    {
+        printf("Please Enter The Number Of Your Choice : ");
+        scanf("%d", &value);
+
+        if (value == 0)
+        {
+            zero = zero + 1;
+        }
+        else if (value > 0)
+        {
+            positive = positive + 1;
+        }
+        else
+        {
+            negative = negative + 1;
+        }
+    }
+
+    printf("The Number Of Positive Numbers Entered Are : %d\n", positive);
+    printf("The Number Of Negative Numbers Entered Are : %d\n", negative);
+    printf("The Number Of Zero's Entered Are : %d", zero);
+
+    return 0;
+}
