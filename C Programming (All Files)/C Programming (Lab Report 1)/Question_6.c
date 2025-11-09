9e59d976dc28ae6df09d489a9007c29441c34b59
Piyush Sindhu <piyush124790@gmail.com>
2025-11-09 22:17:41 +0530

    Question 6 Of C Programming
    Write a C program To Convert Temperature From Celsius To Fahrenheit.

---
+++ C Programming (All Files)/C Programming (Lab Report 1)/Question_6.c
@@ -0,0 +1,18 @@
+// Write a C program To Convert Temperature From Celsius To Fahrenheit.
+
+#include <stdio.h>
+
+int main()
+{
+    float celsius;
+    float fahrenheit;
+
+    printf("Please Enter The Temperature In Celsius : ");
+    scanf("%f", & celsius);
+
+    fahrenheit = (celsius * 9/5) + 32;
+
+    printf("The Temperature In Fahrenheit Is : %.2f", fahrenheit);
+    
+    return 0;
+}
